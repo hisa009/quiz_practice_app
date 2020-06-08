4d56604e5ce7d3680cd2522c872c57a7eb11ee9d
hisa009 <65638907+hisa009@users.noreply.github.com>
2020-06-08 15:59:47 +0900

    Update 20200608064915_create_quizzes.rb

--- db/migrate/20200608064915_create_quizzes.rb
+++ db/migrate/20200608064915_create_quizzes.rb
@@ -1,7 +1,8 @@
 class CreateQuizzes < ActiveRecord::Migration[5.2]
   def change
     create_table :quizzes do |t|
-
+      t.text :text, null: false
+      t.string :answer, null: false
       t.timestamps
     end
   end
