8fb6ffe8c8283ea7008a9276504a8f3ac7ef447b
hisa009 <65638907+hisa009@users.noreply.github.com>
2020-06-10 17:19:19 +0900

    update_create_action_to_quizzes_controller.rb

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -8,5 +8,18 @@ class QuizzesController < ApplicationController
   end
 
   def create
+    quiz = Quiz.new(params_quiz)
+    if quiz.save
+      redirect_to root_path
+      flash[:notice] = '投稿が完了しました'
+    else
+      flash.now[:alert] = '投稿に失敗しました'
+      render :new
+    end
+  end
+
+private
+  def params_quiz
+    params.permit(:text, :answer)
   end
 end
