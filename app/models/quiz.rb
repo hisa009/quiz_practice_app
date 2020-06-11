d85ee9c1355bf63dcabbbabf1bbd6538217153a5
hisa009 <65638907+hisa009@users.noreply.github.com>
2020-06-11 19:32:55 +0900

    add_belongs_to_user_to_quiz.rb

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,4 +1,6 @@
 class Quiz < ApplicationRecord
   validates :text, presence: true, length: { maximum: 60 }
   validates :answer, presence: true, format: { with: /\A[ぁ-ん０-９ー－]+\z/ }
+
+  belongs_to :user
 end
