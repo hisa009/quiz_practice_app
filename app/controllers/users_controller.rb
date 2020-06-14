d506254c70fec93e1194004918dd75627f5aec73
hisa009 <65638907+hisa009@users.noreply.github.com>
2020-06-15 04:30:57 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
+    if user.id != current_user.id
+      flash[:notice] = '権限がありません'
+      redirect_to root_path
+    end
     @quizzes = user.quizzes
   end
 end
