5aba9567a81215d82413a61c7d5ab038be26f206
hisa009 <65638907+hisa009@users.noreply.github.com>
2020-06-15 16:27:57 +0900

    rails_g_controller_correctnesses

---
+++ app/controllers/correctnesses_controller.rb
@@ -0,0 +1,2 @@
+class CorrectnessesController < ApplicationController
+end
---
+++ test/controllers/correctnesses_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CorrectnessesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
