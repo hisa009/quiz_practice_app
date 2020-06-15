e5c1b9cb39b8e92a2c58676a8d31e1b996c4e3e5
hisa009 <65638907+hisa009@users.noreply.github.com>
2020-06-15 10:50:51 +0900

    Update quizzes.js

--- app/assets/javascripts/quizzes.js
+++ app/assets/javascripts/quizzes.js
@@ -1,4 +1,25 @@
 $(function() {
+  var div = function correctAnswer(){
+    let answer = $('.main__form__text').val()
+    if ( answer == gon.answer ) {
+      var html = `<div class="main__answer">
+                    <div class="main__answer__text">正解</div>
+                    <div class="main__answer__link">
+                      <a class="main__answer__link__next" href='#'>やめる</a>
+                      <a class="main__answer__link__next" href='/'>次の問題に進む</a>
+                    </div>
+                  </div>`
+    } else {
+      var html = `<div class="main__answer">
+                    <div class="main__answer__text2">不正解</div>
+                    <div class="main__answer__link">
+                    <a class="main__answer__link__next" href='#'>やめる</a>
+                    <a class="main__answer__link__next" href='/'>次の問題に進む</a>
+                    </div>
+                  </div>`
+    }
+    return html;
+  }
   $(window).on('load', function() {
     $('.main__question__text').children().addBack().contents().each(function() {
       if (this.nodeType == 3) {
@@ -10,7 +31,12 @@ $(function() {
       $('.main__question__text').children('span:eq('+i+')').delay(200*i).animate({ 'opacity': 1 }, 50);
     };
   });
+
   $('.main__btn').click(function(){
     $('.main__question__text span').stop(true, false);
   });
+  
+  $('.main__form__btn').click(function() {
+    $('.main').append(div)
+  })
 });
