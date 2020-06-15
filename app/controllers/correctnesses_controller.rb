fd51a7542d228f1c7abbabeec0438a639c99b05e
hisa009 <65638907+hisa009@users.noreply.github.com>
2020-06-15 16:34:26 +0900

    Update correctnesses_controller.rb

--- app/controllers/correctnesses_controller.rb
+++ app/controllers/correctnesses_controller.rb
@@ -1,2 +1,15 @@
 class CorrectnessesController < ApplicationController
+  def create
+    Correctness.create(correctness_params)
+  end
+end
+
+private
+def correctness_params
+  quiz = Quiz.find(params[:quiz_id])
+  if quiz.answer == params[:text]
+    params.permit(:quiz_id).merge(answer_flg: true)
+  else
+    params.permit(:quiz_id).merge(answer_flg: false)
+  end
 end
